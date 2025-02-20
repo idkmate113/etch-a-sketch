5b8b2794dd2b904d81f64f7cc6818d4b9c294a11
id3mate <179154170+idkmate113@users.noreply.github.com>
2025-02-20 16:34:12 +0100

    Implemented reset button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,6 +2,7 @@ let container = document.querySelector("#container");
 let etch = document.createElement("div");
 let input = document.querySelector("input");
 let button = document.querySelector(".button");
+let resetBtn = document.querySelector(".buttonReset");
 etch.classList.add("etch");
 
 let userInput = prompt("How many grids do you wanna make?", 16);
@@ -11,9 +12,31 @@ container.appendChild(etch);
 function removeGrid() {
   etch.innerHTML = "";
 }
+resetBtn.addEventListener("click", () => {
+  removeGrid();
+  for (let i = 0; i < userInput * userInput; i++) {
+    while (userInput > 100) {
+      userInput = prompt("Can't make more than 100 grids", 16);
+    }
+    let divs = document.createElement("div");
+
+    divs.classList.add("divs");
+    container.appendChild(divs);
+    etch.appendChild(divs);
+    divs.style.height = 700 / userInput + "px";
+    divs.style.width = 700 / userInput + "px";
+
+    divs.addEventListener("mouseover", () => {
+      divs.style.backgroundColor = input.value;
+    });
+  }
+});
 
 function makingGrid() {
   for (let i = 0; i < userInput * userInput; i++) {
+    while (userInput > 100) {
+      userInput = prompt("Can't make more than 100 grids", 16);
+    }
     let divs = document.createElement("div");
 
     divs.classList.add("divs");
@@ -33,10 +56,11 @@ function makingGrid() {
 
   function createGrid() {
     userInput = prompt("How many grids do you wanna make?", 16);
-    if (userInput > 100) {
-      userInput = prompt("Can't make more than 100 grids", 16);
-    }
+
     for (let i = 0; i < userInput * userInput; i++) {
+      while (userInput > 100) {
+        userInput = prompt("Can't make more than 100 grids", 16);
+      }
       let divs = document.createElement("div");
 
       divs.classList.add("divs");
